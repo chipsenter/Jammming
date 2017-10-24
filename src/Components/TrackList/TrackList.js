c872bc4d46635a00f796eb9af4e1ac93f8448094
Johan <jlindstrom@lifetimefitness.com>
2017-10-24 15:32:21 -0600

    1st part of the project is rendering

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -3,7 +3,12 @@ import './TrackList.css';
 import Track from '../Track/Track';
 
 class TrackList extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render(){
+        console.log(this.props);
          return(
             <div className="TrackList">
                 {this.props.tracks.map(track => (
@@ -16,4 +21,5 @@ class TrackList extends React.Component {
         );
     }
 }
+
 export default TrackList;
